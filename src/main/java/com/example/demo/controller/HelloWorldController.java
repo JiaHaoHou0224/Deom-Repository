2e19e7bef2d9fc46bfa3e06bf8fcfd6a53718799
后家豪 <JiaHao_Hou@yeah.net>
2022-10-30 21:06:13 +0800

    测试代码提交

---
+++ src/main/java/com/example/demo/controller/HelloWorldController.java
@@ -0,0 +1,14 @@
+package com.example.demo.controller;
+
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController //web控制器构造型注释，响应浏览器请求。
+public class HelloWorldController {
+
+    @RequestMapping("/")
+    public String index() {
+        return "Hello, World!";
+    }
+}
